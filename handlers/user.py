22bc557f30bc874f4d21c835671066aa1eb4d39b
JOGAS <narkata528@gmail.com>
2026-08-29 19:22:43 +0300

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -296,19 +296,18 @@ async def menu_chats(callback: CallbackQuery) -> None:
         return
 
        await callback.message.answer(
-        """<tg-emoji emoji-id="5870886806601338791">📋</tg-emoji> <b>Информация о проекте MTR</b>
-        └ Дата запуска: 28.08.2026
+    """📋 Информация о проекте MTR
+└ Дата запуска: 28.08.2026
 
-        <b>Процент выплат:</b>
-        ├ Прямой перевод: 80%
-        ├ Крипто деп: 80%
-        ├ Пополнение: 80%
-        └ Тех. поддержка: 70%""",
-    reply_markup=kb.chats_list_kb(chats),
-    parse_mode="HTML"
-    )
+Процент выплат:
+├ Прямой перевод: 80%
+├ Крипто деп: 80%
+├ Пополнение: 80%
+└ Тех. поддержка: 70%""",
+    reply_markup=kb.chats_list_kb(chats)
+)
 
-        await callback.answer()
+await callback.answer()
 
 
 # ---------------------------------------------------------------------------
