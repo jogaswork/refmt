05eae397f5365fb0910f67f64157ff914177bd17
JOGAS <narkata528@gmail.com>
2026-08-30 05:08:32 +0300

    Update user.py

--- handlers/user.py
+++ handlers/user.py
@@ -304,16 +304,17 @@ async def menu_chats(callback: CallbackQuery) -> None:
         await callback.answer()
         return
 
-    await callback.message.answer (
-f"Информация о проекте MTR TEAM\n\n"
-"└ Дата запуска: 28.08.2026\n\n"
-"Процент выплат:\n"
-"├ Прямой перевод: 80%\n"
-"├ Крипто деп: 80%\n"
-"├ Пополнение: 80%\n"
-"└ Тех. поддержка: 70%"
-)reply_markup=kb.chats_list_kb(chats)
-    await callback.answer()
+await callback.message.answer(
+    f"Информация о проекте MTR TEAM\n\n"
+    "└ Дата запуска: 28.08.2026\n\n"
+    "Процент выплат:\n"
+    "├ Прямой перевод: 80%\n"
+    "├ Крипто деп: 80%\n"
+    "├ Пополнение: 80%\n"
+    "└ Тех. поддержка: 70%",
+    reply_markup=kb.chats_list_kb(chats)
+)
+await callback.answer()
 
 
 # ---------------------------------------------------------------------------
